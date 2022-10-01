bad482a5abeb7f6937305b11a63b0afd8331550c
sachieko <arashikabane@gmail.com>
2022-10-01 17:25:00 -0600

    Basic chat component with PMs

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,46 @@
 const { Server } = require('socket.io');
 const express = require('express');
 const app = express();
+const { uniqueNamesGenerator, starWars } = require('unique-names-generator');
 
 const http = app.listen(8080, () => {
   console.log(`Server running at port: 8080`);
 });
 
+const clients = {};
 const io = new Server(http);
 
 io.on('connection', client => {
-  console.log("Client Connected!");
-  client.on("disconnect", () => {
-    console.log("Client Disconnected");
+  const name = uniqueNamesGenerator({
+    dictionaries: [starWars]
+  });
+
+  console.log('Client Connected!', name, ':', client.id);
+
+  client.emit('system', `Arr, welcome to the room ${name}!`);
+  client.broadcast.emit('system', `Arr, ye've been boarded by ${name}!`);
+
+  clients[name] = client.id;  // Add client to lookup object. We will need to do more work to implement private rooms.
+  // console.log('Clients: ', clients);
+
+
+  client.on('message', data => {
+    // console.log('Data received:', data);
+    const {msg, to} = data
+    const username = name;
+    if (!to) {
+      client.emit('public', {msg, username});
+      client.broadcast.emit('public', {msg, username});
+      return;
+    }
+    const id = clients[to];
+    // console.log(`Sending message to ${to}:${id}`);
+    io.to(id).emit('private', {msg, username});
+  })
+
+  client.on('disconnect', () => {
+    console.log('Client Disconnected', name, ':', client.id);
+    client.broadcast.emit('system', `${name} has just walked the plank!`);
+    delete clients[name];
   });
 });
