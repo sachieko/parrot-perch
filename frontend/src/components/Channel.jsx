59dd8eed71fc4dfd120dc9680f04074c10de01db
Ryan <ryanwfortin@gmail.com>
2022-10-03 00:16:17 -0700

    import scss for channel display

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
+import "./Channel.scss";
+
 export default function Channel(props) {
- 
+  const url = `http://www.twitch.tv/${props.display_name}`
+
   return (
-    <article className="album">
-      <img className="album__thumbnail" src={props.thumbnail_url} alt="Channel" />
-      <div className='random'>
-        <div className="album__name">{props.display_name}</div>
-        <div className="album__artist">{props.game_name}</div>
+    <article className="streamer-channel" onClick={event =>  window.location.href=url}>
+      <img className="thumbnail" src={props.thumbnail_url} alt="Channel" />
+      <div className="names">
+        <div className="display_name">{props.display_name}</div>
+        <div className="game_name">{props.game_name}</div>
       </div>
     </article>
   );
