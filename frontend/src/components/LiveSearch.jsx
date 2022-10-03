ceeec06aa3d67292710e479c9aae4523d30bdd1e
Ryan <ryanwfortin@gmail.com>
2022-10-03 00:17:32 -0700

    refactor promises

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -8,55 +8,47 @@ export default function LiveSearch(props) {
   const [results, setResults] = useState([]);
   let token = '';
 
-useEffect(() => {
-  const testURL = `https://api.twitch.tv/helix/search/channels?query=${term}`;
-  axios.post('https://id.twitch.tv/oauth2/token', {
-    client_id: process.env.REACT_APP_CLIENT_ID,
-    client_secret: process.env.REACT_APP_CLIENT_SECRET,
-    grant_type: process.env.REACT_APP_GRANT_TYPE
-})
-  .then(response => {
-    token = response.data.access_token
+  useEffect(() => {
+    const testURL = `https://api.twitch.tv/helix/search/channels?query=${term}`;
+    axios.post('https://id.twitch.tv/oauth2/token', {
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      client_secret: process.env.REACT_APP_CLIENT_SECRET,
+      grant_type: process.env.REACT_APP_GRANT_TYPE
+    })
+      .then(response => {
+        token = response.data.access_token
 
-    axios.get('https://id.twitch.tv/oauth2/validate', {
-      headers: {
-      'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => {
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-
-})
-  .then(response => {
-    axios.get(testURL, {
-      headers: {
-      'Authorization': `Bearer ${token}`, 
-      'Client-Id': process.env.REACT_APP_CLIENT_ID
-      }
-    })
-    .then(response => {
-      // results = response.data
-      setResults([...response.data.data])
-      console.log(results);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  })
-  .catch((e) => {
-    console.log(e);
-});
-
-}, [term]);
+        axios.get('https://id.twitch.tv/oauth2/validate', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+      })
+      .then(response => {
+        axios.get(testURL, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Client-Id': process.env.REACT_APP_CLIENT_ID
+          }
+        })
+          .then(response => {
+            setResults([...response.data.data])
+            console.log(results);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [term]);
 
   return (
     <Fragment>
       <main>
         <SearchBar onSearch={setTerm} />
-        <Results results={results} />
+        <Results results={results}  />
       </main>
     </Fragment>
   );
