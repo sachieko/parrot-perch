0fcee76a3e141c90e80945837a7524ee4c31011b
Ryan <ryanwfortin@gmail.com>
2022-10-03 19:20:08 -0700

    adjust component name

--- frontend/src/components/viewRoom.jsx
+++ frontend/src/components/viewRoom.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ChangeChannel from './changeChannel';
+import ChangeChannel from './ChangeChannel';
 import LiveSearch from './LiveSearch';
 
 function ViewRoom(props) {
