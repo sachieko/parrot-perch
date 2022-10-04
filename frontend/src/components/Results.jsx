4f4a5f845ae66674a4338027b0a324777cba64b9
Ryan <ryanwfortin@gmail.com>
2022-10-03 18:49:54 -0700

    pass down handleChannel function

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -6,6 +6,8 @@ export default function Results(props) {
   const { results } = props;
 
   return results.map(stream => {
-    return <Channel key={stream.id} {...stream} />;
+    return <Channel key={stream.id} {...stream} 
+      handleChannel={props.handleChannel}
+    />;
   });
 }
