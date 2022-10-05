e611e3d70f5f421b901b1860e76972887ac4d012
Ryan <ryanwfortin@gmail.com>
2022-10-05 13:46:32 -0700

    add password input

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,6 +1,6 @@
-import RoomPassword from "./RoomPassword";
-
 function Join (props) {
+
+  console.log(props)
   return (
     <main>
     <form>
@@ -8,9 +8,12 @@ function Join (props) {
         Join Room:
         <input type="text" name="roomName" value={props.value} onChange={props.onChange} />
       </label>
+      <label>
+        Password?
+      <input type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
+      </label>
       <input type="submit" value="Submit" onClick={props.onClick} />
     </form>
-    <RoomPassword />
     </main>
   )
 }
