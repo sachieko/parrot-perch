6d010ca38cf43ff66ac9e5d292284dcc4b70b4ef
Anthony Campbell <anthony-campbell@hotmail.com>
2022-10-04 21:30:33 -0700

    Youtube embedded in rooms.

--- frontend/src/components/apis/Youtube/index.jsx
+++ frontend/src/components/apis/Youtube/index.jsx
@@ -1,7 +1,33 @@
+import YoutubePlayer from 'react-youtube';
+import { useState } from 'react';
+
 function Youtube() {
+  const [player, setPlayer] = useState();
+
+  const opts = useState({
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      start: 0
+    }
+  })[0];
+
+  const onReady = (event) => {
+    setPlayer(event.target);
+  }
+
+  const seek = (event) => {
+    player.seekTo(9);
+  }
+
   return (
-    <p>Youtube</p>
-  )
+    <div>
+      <YoutubePlayer videoId="2g811Eo7K8U" opts={opts} onReady={onReady} />
+      <button onClick={seek}>seek</button>
+    </div>
+  );
 }
 
 export default Youtube;
