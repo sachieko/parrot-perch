bae087066690eaca7d5435af4b53fe6acdb72080
Ryan <ryanwfortin@gmail.com>
2022-10-06 15:26:38 -0700

    adjust text

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -9,7 +9,7 @@ function Join (props) {
         <input type="text" name="roomName" value={props.value} onChange={props.onChange} />
       </label>
       <label>
-        Password?
+        Password:
       <input type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
       </label>
       <input type="submit" value="Submit" onClick={props.onClick} />
