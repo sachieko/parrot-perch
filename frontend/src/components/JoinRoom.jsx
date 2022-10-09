d5858c56dd00da0da211f18f4a0b78c73c575ed9
Ryan <ryanwfortin@gmail.com>
2022-10-09 15:02:18 -0700

    adjust root page style

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,16 +1,16 @@
 function Join (props) {
 
   return (
-    <form>
+    <form className="root-inputs">
       <label>
         Join Room:
       </label>
-        <input type="text" name="roomName" value={props.value} onChange={props.onChange} />
+        <input className='radius' type="text" name="roomName" value={props.value} onChange={props.onChange} />
       <label>
         Password:
       </label>
-      <input type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
-      <input type="submit" value="Submit" onClick={props.onClick} />
+      <input className="radius" type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
+      <input className="radius" type="submit" value="Submit" onClick={props.onClick} />
     </form>
   )
 }
