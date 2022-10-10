d93c5b34a7698025e3a62c7b66099a0db32e9faf
sachieko <arashikabane@gmail.com>
2022-10-10 00:09:22 -0600

    Resize elements for tablet, fix chat message wrapping

--- frontend/src/components/Chat/ChatInput.jsx
+++ frontend/src/components/Chat/ChatInput.jsx
@@ -4,23 +4,27 @@ import { roomContext } from '../../providers/RoomProvider';
 const ChatInput = props => {
   const { to, setTo } = useContext(roomContext);
 
-  const options = props.users.map(user => {
-    return <option className='user-list-item' style={{color: user.color}} value={user.name}>{user.name}</option>
+  const options = props.users.map((user, i) => {
+    return <option key={i} className='user-list-item' style={{color: user.color}} value={user.name}>{user.name}</option>
   });
   
   return (
-    <div className="chat-input">
-      <input
-        onChange={event => props.onChange(event.target.value)}
-        value={props.value}
-        placeholder="Chat something!"
-        onKeyUp={event => event.key === 'Enter' && props.value ? props.send() : null} id='chat-submit' />
+    <div className="chat-input-container">
       <div className="chat-pm">
         <button onClick={() => setTo('')}>Reset PM</button>
         <select onChange={(event) => setTo(event.target.value)} value={to} >
           <option className='user-list-item' value="">Select a friend</option>
           {options}
         </select>
+        
+      </div>
+      <div className="chat-input">
+      <input
+        onChange={event => props.onChange(event.target.value)}
+        value={props.value}
+        placeholder="Chat something!"
+        autoComplete="off"
+        onKeyUp={event => event.key === 'Enter' && props.value ? props.send() : null} id='chat-submit' />
         <button onClick={props.send}>Chat</button>
       </div>
     </div>
--- frontend/src/components/Chat/PrivateMessage.jsx
+++ frontend/src/components/Chat/PrivateMessage.jsx
@@ -7,16 +7,20 @@ const PrivateMessage = (props) => {
     <>
     {pm === 'receive' && (
     <div className='chat-list-item'>
-        <span className='message-item'>[PM from </span>
-        <span className='chat-username-item' style={{ color }}>{username}</span>
-        <span className='message-item'>]: {message}</span>
+        <span className='message-item'>{`[PM from `}
+          <span className='chat-username-item' style={{ color }}>{username}</span>
+          {`]: `}
+        </span>
+        <span className='message-item'>{message}</span>
     </div>
     )}
     {pm === 'send' && (
     <div className='chat-list-item'>
-        <span className='message-item'>[PM to </span>
-        <span className='chat-username-item' style={{ color }}>{username}</span>
-        <span className='message-item'>]: {message}</span>
+        <span className='message-item'>{`[PM to `}
+          <span className='chat-username-item' style={{ color }}>{username}</span>
+          {`]: `}
+        </span>
+        <span className='message-item'> {message}</span>
     </div>
     )}
     </>
