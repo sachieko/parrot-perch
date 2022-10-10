922430048689197d620ce19978e8cda6516493e1
Ryan <ryanwfortin@gmail.com>
2022-10-09 22:03:28 -0700

    adjust text

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -3,13 +3,12 @@ function Join (props) {
   return (
     <form className="root-inputs">
       <label>
-        Join Room:
+        -Create or Join a Room-
       </label>
-        <input className='radius' type="text" name="roomName" value={props.value} onChange={props.onChange} />
+        <input className='radius' placeholder="Enter Room Name" type="text" name="roomName" value={props.value} onChange={props.onChange} />
       <label>
-        Password:
       </label>
-      <input className="radius" type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
+      <input className="radius" placeholder='Enter Password' type="password" name="roomPassword" value={props.value2} onChange={props.onChange2} />
       <input className="radius" type="submit" value="Submit" onClick={props.onClick} />
     </form>
   )
