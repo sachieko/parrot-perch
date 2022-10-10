3fcb561fb370e7a9f8b2eeddcc9972d9112fc500
Ryan <ryanwfortin@gmail.com>
2022-10-09 22:45:03 -0700

    import navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,13 @@ import './App.scss';
 import Room from './components/Room'
 import RoomProvider from './providers/RoomProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 const App = function () {
 
   return (
     <div className="parrot-perch">
-      <nav className='nav'>
-        <a href='/' className='nav-link'>
-          <h1 id='app-header'>Parrot Perch</h1>
-        </a>
-      </nav>
+      <NavBar />
       <RoomProvider>
         <Room />
       </RoomProvider>
