71868c473e5000037bf9b0cd0c1f7f45bea23bdd
sachieko <arashikabane@gmail.com>
2022-10-10 00:21:09 -0600

    Get rid of console log that shouldn't exist, how did it get there?

--- frontend/src/components/Chat/PrivateMessage.jsx
+++ frontend/src/components/Chat/PrivateMessage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const PrivateMessage = (props) => {
   const { username, color, message, pm } = props;
-  console.log(props);
   return (
     <>
     {pm === 'receive' && (
