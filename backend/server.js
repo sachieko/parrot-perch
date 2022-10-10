f5b8a36518d43a738c52b2376c3d011adb9779cb
Anthony Campbell <anthony-campbell@hotmail.com>
2022-10-10 01:17:13 -0700

    Increase youtube max search results.

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ app.get('/api/twitch_search', (req, res) => {
 // Youtube search
 app.get('/api/youtube_search', (req, res) => {
   const terms = req.query.terms;
-  const searchURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${terms}&key=${process.env.YOUTUBE_API_KEY}`;
+  const searchURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${terms}&maxResults=20&key=${process.env.YOUTUBE_API_KEY}`;
 
   axios.get(searchURL).then(response => {
     res.send(response.data);
