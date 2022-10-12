a727c66ffd93d155da50798a6274bc15c6f21e54
sachieko <arashikabane@gmail.com>
2022-10-11 19:44:17 -0600

    rename to className, css for mobile views

--- frontend/src/components/apis/Twitch/MinimizeToggle.jsx
+++ frontend/src/components/apis/Twitch/MinimizeToggle.jsx
@@ -1,8 +1,8 @@
 const MinimizeToggle = (props) => {
   if (props.bool) {
-    return (<><i className="fa-solid fa-window-minimize"/> <i class="fa-regular fa-comment" /></>);
+    return (<><i className="fa-solid fa-window-minimize"/> <i className="fa-regular fa-comment" /></>);
   }
-  return (<><i className="fa-solid fa-plus" /> <i class="fa-regular fa-comment" /></>);
+  return (<><i className="fa-solid fa-plus" /> <i className="fa-regular fa-comment" /></>);
 };
 
 export default MinimizeToggle;
