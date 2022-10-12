2b6444dc22e384c9466b8a2d9700b093526f62de
sachieko <arashikabane@gmail.com>
2022-10-11 19:56:27 -0600

    Fix minimize button style for all views

--- frontend/src/components/apis/Twitch/MinimizeToggle.jsx
+++ frontend/src/components/apis/Twitch/MinimizeToggle.jsx
@@ -1,6 +1,6 @@
 const MinimizeToggle = (props) => {
   if (props.bool) {
-    return (<><i className="fa-solid fa-window-minimize"/> <i className="fa-regular fa-comment" /></>);
+    return (<><i className="fa-solid fa-minus"/> <i className="fa-regular fa-comment" /></>);
   }
   return (<><i className="fa-solid fa-plus" /> <i className="fa-regular fa-comment" /></>);
 };
